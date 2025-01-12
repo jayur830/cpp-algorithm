d2b8e0421a6fd20453e41eb12cac162be08b5146
jayur830 <jayur830@naver.com>
2025-01-12 18:18:00 +0900

    style: 주석 추가

--- backjoon/10808.cpp
+++ backjoon/10808.cpp
@@ -5,6 +5,10 @@
 char s[101];
 int n[ALPHABET_COUNT] { 0, };
 
+/**
+ * 1. 알파벳 총 개수만큼의 int 배열 선언 후
+ * 2. ASCII 'a'를 뺀 숫자로 인덱싱하여 카운팅한다.
+ */
 int main() {
     scanf("%s", s);
     for (int i = 0; i < strlen(s); i++) {
