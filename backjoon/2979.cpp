8c80a7216986862ba208ed11eadc31d430c2ec73
jayur830 <jayur830@naver.com>
2025-01-12 18:14:39 +0900

    feat: 2979 트럭 주차
    ref: https://www.acmicpc.net/problem/2979

---
+++ backjoon/2979.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int p[3], s, e, t[101] { 0, }, sum = 0, m = -999;
+
+int main() {
+    scanf("%d %d %d", &p[0], &p[1], &p[2]);
+    for (int i = 0; i < 3; i++) {
+        scanf("%d %d", &s, &e);
+        m = m < e ? e : m;
+        for (int j = s; j < e; j++) {
+            t[j]++;
+        }
+    }
+    for (int i = 1; i <= m; ++i) {
+        if (t[i] > 0) {
+            sum += p[t[i] - 1] * t[i];
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
+}
